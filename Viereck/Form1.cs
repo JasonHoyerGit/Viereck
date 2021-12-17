33abc5e4efefd34cef0e30034c75d721c1bdfb38
Fuchs <hoyerjason0@gmail.com>
2021-12-17 12:32:07 +0100

    Projektdateien hinzufügen.

---
+++ Viereck/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Viereck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Rectangle rectangle;
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            if (rectangle != null)
+            {
+                graphics.FillRectangle(blueBrush, rectangle);
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point point1 = e.Location;
+            rectangle = new Rectangle(point1.X, point1.Y, 100, 100);
+
+            Refresh();
+        }
+    }
+}
